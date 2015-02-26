9393632ab5e502e7bd4e0db67a74d9f8652bfedb
majiajia <hnxymjj@163.com>
2015-02-26 21:14:44 +0800

    注册和登录
    注册和登录的基本结构

--- main.py
+++ main.py
@@ -7,21 +7,27 @@ import tornado.options
 import tornado.web
 
 from tornado.options import define,options
-define("port",default=8888,help="run on the given port",type=int)
+define("port",default=9000,help="run on the given port",type=int)
 
 
 class LoginHandler(tornado.web.RequestHandler):
     def get(self):
-        self.render("test.html")
+        self.render("login.html")
+
+class RegisterHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("cdcdcd")
 
 if __name__ == '__main__':
     tornado.options.parse_command_line()
     app = tornado.web.Application(
         handlers = [
             (r"/",LoginHandler),
-            (r"/",LoginHandler)
+            (r'/home/reg',RegisterHandler)
+
         ],
-        template_path = os.path.join(os.path.dirname(__file__),"template")
+        template_path = os.path.join(os.path.dirname(__file__),"template"),
+        static_path=os.path.join(os.path.dirname(__file__),"static")
     )
     http_server = tornado.httpserver.HTTPServer(app)
     http_server.listen(options.port)
